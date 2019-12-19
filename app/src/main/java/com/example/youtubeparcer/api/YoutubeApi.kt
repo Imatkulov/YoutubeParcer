ae91bacc8028d2114700b0e840ed08b9149746a7
ser1888 <ser1888kg@gmail.com>
2019-12-19 17:52:18 +0600

    add network files

---
+++ app/src/main/java/com/example/youtubeparcer/api/YoutubeApi.kt
@@ -0,0 +1,18 @@
+package com.example.youtubeparcer.api
+
+import com.example.youtubeparcer.model.PlaylistModel
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+
+interface YoutubeApi {
+
+    @GET("youtube/v3/playlists")
+    fun getPlaylists(
+        @Query("part") part: String,
+        @Query("key") apiKey: String,
+        @Query("channelId") channelId: String,
+        @Query("maxResults") maxResults: String
+    ): Call<PlaylistModel>
+}
